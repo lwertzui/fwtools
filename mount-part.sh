39f2800ae2122b593cd8de5ac8bbe908dda43828
Wertzui <122686226+lwertzui@users.noreply.github.com>
2025-07-21 21:29:33 +0200

    Update mount-part.sh

--- mount-part.sh
+++ mount-part.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+#mount-part
+#Usage: mount-part [FILE]
+# mount binary file system to /mnt/[FILE]
+#Example: ./mount-part part1.bin
 
 SRC=$1
-
-umount /mnt/tst
-mount -o loop $SRC /mnt/tst || mount $SRC /mnt/tst
-mc /mnt/tst
-umount /mnt/tst
-
+umount /mnt/$1 || mkdir /mnt/$1 
+mount -o loop $SRC /mnt/$1 || mount $SRC /mnt/$1
+cd /mnt/$1 || exit 1
+exec bash
